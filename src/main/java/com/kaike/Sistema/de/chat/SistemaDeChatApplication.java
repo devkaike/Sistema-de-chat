04500f4e992d9e53e31154c2fc96e1e60e79348c
kaike silva rocha <skaike718@gmail.com>
2024-10-28 18:29:54 -0300

    Primeiro commit

---
+++ src/main/java/com/kaike/Sistema/de/chat/SistemaDeChatApplication.java
@@ -0,0 +1,13 @@
+package com.kaike.Sistema.de.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaike/Sistema/de/chat/SistemaDeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaike.Sistema.de.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
